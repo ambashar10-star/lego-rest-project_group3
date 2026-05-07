73a8b1e50c7c86745aa3c1e6f5dfee60893bc088
Nevindi <nevindirasanja@gmail.com>
2026-05-07 09:28:25 +0300

    Enable LightCheck and its thread in LegoApp

--- TWOPROPERTIES/src/app/LegoApp.java
+++ TWOPROPERTIES/src/app/LegoApp.java
@@ -6,24 +6,27 @@ public class LegoApp {
 
     public static void main(String[] args) {
 
+        // creating objects
         RunLego runLego = new RunLego();
         ReadData readData = new ReadData();
         SendData sendData = new SendData();
         SensorCheck sensorCheck = new SensorCheck();
-        //LightCheck lightCheck = new LightCheck();
+        LightCheck lightCheck = new LightCheck();
 
         System.out.println("Run in Threads");
 
+        // creating threads for the objects
         Thread runLegoThread = new Thread(runLego);
         Thread readDataThread = new Thread(readData);
         Thread sendDataThread = new Thread(sendData);
         Thread sensorThread = new Thread(sensorCheck);
-        //Thread lightThread = new Thread(lightCheck);
+        Thread lightThread = new Thread(lightCheck);
 
+        // starting the threads
         runLegoThread.start();
         readDataThread.start();
         sendDataThread.start();
         sensorThread.start();
-        //lightThread.start();
+        lightThread.start();
     }
 }
